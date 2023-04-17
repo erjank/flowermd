afa6d6d4b87ab8eec56848798b0d6a9e4534b715
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2023-04-17 10:03:20 -0600

    add shear sim class

--- hoomd_polymers/sim/tensile.py
+++ hoomd_polymers/sim/tensile.py
@@ -5,12 +5,132 @@ from hoomd_polymers.sim.simulation import Simulation
 from hoomd_polymers.sim.actions import PullParticles
 
 
+class Shear(Simulation):
+    def __init__(
+            self,
+            initial_state,
+            forcefield,
+            shear_axis,
+            interface_axis,
+            fix_ratio=0.20,
+            r_cut=2.5,
+            dt=0.0001,
+            device=hoomd.device.auto_select(),
+            seed=42,
+            restart=None,
+            gsd_write_freq=1e4,
+            gsd_file_name="trajectory.gsd",
+            log_write_freq=1e3,
+            log_file_name="sim_data.txt"
+    ):
+        super(Shear, self).__init__(
+                initial_state=initial_state,
+                forcefield=forcefield,
+                r_cut=r_cut,
+                dt=dt,
+                device=device,
+                seed=seed,
+                gsd_write_freq=gsd_write_freq,
+                gsd_file_name=gsd_file_name,
+                log_write_freq=log_write_freq,
+        )
+        self.shear_axis = shear_axis
+        self.interface_axis = shear_axis
+        self.fix_ratio = fix_ratio
+        axis_array_dict = {
+                "x": np.array([1,0,0]),
+                "y": np.array([0,1,0]),
+                "z": np.array([0,0,1])
+        }
+        axis_dict = {"x": 0, "y": 1, "z": 2}
+        self._shear_axis_index = axis_dict[self.shear_axis]
+        self._shear_axis_array = axis_array_dict[self.shear_axis]
+        self._interface_axis_index = axis_dict[self.interface_axis]
+        self._interface_array = axis_array_dict[self.interface_axis]
+
+        self.initial_box = self.box_lengths_reduced
+        self.initial_length = self.initial_box[self._shear_axis_index]
+        self.fix_length = self.initial_length * fix_ratio
+
+        # Set up walls of fixed particles:
+        snapshot = self.state.get_snapshot()
+        shear_positions = snapshot.particles.position[:,self._shear_axis_index]
+        interface_positions = snapshot.particles.position[
+                :,self._interface_axis_index
+        ]
+        box_max = self.initial_length / 2
+        box_min = -box_max
+
+        # Set tag groups for filters and particle shifts
+        shear_neg_tags = np.where(shear_positions<(box_min+self.fix_length))[0]
+        shear_pos_tags = np.where(shear_positions>(box_max-self.fix_length))[0]
+        all_shear_tags = np.intersect1d(shear_neg_tags, shear_pos_tags)
+        interface_neg_tags = np.where(interface_positions < 0)[0]
+        interface_pos_tags = np.where(interface_positions > 0)[0]
+
+        shift_up_tags = np.intersect1d(interface_neg_tags, all_shear_tags) 
+        shift_down_tags = np.intersect1d(interface_pos_tags, all_shear_tags) 
+        
+        # Create hoomd filters
+        self.fix_shift_up = hoomd.filter.Tags(shift_up_tags) 
+        self.fix_shift_down = hoomd.filter.Tags(shift_down_tags) 
+        all_fixed = hoomd.filter.Union(self.fix_shift_up, self.fix_shift_down)
+        # Set the group of particles to be integrated over
+        self.integrate_group = hoomd.filter.SetDifference(
+                hoomd.filter.All(), all_fixed
+        )
+        self.add_walls(
+                wall_axis=self._interface_axis_array,
+                sigma=1.0,
+                epsilon=1.0,
+                r_cut=2.5
+        )
+
+    def run_shear(self, strain, kT, n_steps, period):
+        current_length = self.box_lengths_reduced[self._shear_axis_index]
+        final_length = current_length * (1+strain)
+        final_box = np.copy(self.box_lengths_reduced)
+        final_box[self._shear_axis_index] = final_length
+        shift_by = (final_length - current_length) / (n_steps//period)
+        resize_trigger = hoomd.trigger.Periodic(period)
+        box_ramp = hoomd.variant.Ramp(
+                A=0, B=1, t_start=self.timestep, t_ramp=int(n_steps)
+        )
+        box_resizer = hoomd.update.BoxResize(
+                box1=self.box_lengths_reduced,
+                box2=final_box,
+                variant=box_ramp,
+                trigger=resize_trigger,
+                filter=hoomd.filter.Null()
+        )
+        particle_puller = PullParticles(
+            shift_by=shift_by/2,
+            axis=self._shear_axis_array,
+            neg_filter=self.fix_shift_down,
+            pos_filter=self.fix_shift_up
+        )
+        particle_updater = hoomd.update.CustomUpdater(
+                trigger=resize_trigger, action=particle_puller
+        )
+        wall_update = UpdateWalls(sim=self)
+        wall_updater = hoomd.update.CustomUpdater(
+                trigger=resize_trigger, action=wall_update
+        )
+        self.operations.updaters.append(box_resizer)
+        self.operations.updaters.append(particle_updater)
+        self.set_integrator_method(
+            integrator_method=hoomd.md.methods.NVE,
+            method_kwargs={"filter": self.integrate_group}
+        )
+        self.run(n_steps + 1)
+
+
 class Tensile(Simulation):
     def __init__(
             self,
             initial_state,
             forcefield,
-            tensile_axis,
+            tensile_axis,  # Tensile axis == Interface Axis
             fix_ratio=0.20,
             r_cut=2.5,
             dt=0.0001,
