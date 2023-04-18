25df2b5a887f5bdb824ed8497c096ad6943e2771
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2023-04-18 11:41:50 -0600

    trying out shear using constant forces

--- hoomd_polymers/sim/mechanical.py
+++ hoomd_polymers/sim/mechanical.py
@@ -5,6 +5,81 @@ from hoomd_polymers.sim.simulation import Simulation
 from hoomd_polymers.sim.actions import PullParticles, UpdateWalls
 
 
+class ShearForce(Simulation):
+    def __init__(
+            self,
+            initial_state,
+            forcefield,
+            shear_axis,
+            interface_axis,
+            shear_force=1,
+            fix_ratio=0.20,
+            r_cut=2.5,
+            dt=0.0001,
+            device=hoomd.device.auto_select(),
+            seed=42,
+            restart=None,
+            gsd_write_freq=1e4,
+            gsd_file_name="shear.gsd",
+            log_write_freq=1e3,
+            log_file_name="sim_data.txt"
+    ):
+        super(Shear, self).__init__(
+                initial_state=initial_state,
+                forcefield=forcefield,
+                r_cut=r_cut,
+                dt=dt,
+                device=device,
+                seed=seed,
+                gsd_write_freq=gsd_write_freq,
+                gsd_file_name=gsd_file_name,
+                log_write_freq=log_write_freq,
+        )
+        self.shear_axis = np.asarray(shear_axis)
+        self.interface_axis = np.asarray(interface_axis)
+        self.shear_force = shear_force
+        self.fix_ratio = fix_ratio
+        self._shear_axis_index = np.where(self.shear_axis != 0)[0]
+        self._interface_axis_index = np.where(self.interface_axis != 0)[0]
+
+        snapshot = self.state.get_snapshot()
+        interface_positions = snapshot.particles.position[
+                :,self._interface_axis_index
+        ]
+        interface_neg_tags = np.where(interface_positions < 0)[0]
+        interface_pos_tags = np.where(interface_positions > 0)[0]
+
+        shift_up_tags = interface_neg_tags
+        shift_down_tags = interface_pos_tags
+
+        # Create hoomd filters
+        self.shift_up = hoomd.filter.Tags(shift_up_tags.astype(np.uint32))
+        self.shift_down = hoomd.filter.Tags(shift_down_tags.astype(np.uint32))
+        self.add_walls(
+                wall_axis=self.interface_axis,
+                sigma=1.0,
+                epsilon=1.0,
+                r_cut=1.5
+        )
+        up_force = hoomd.md.force.Constant(filter=self.shift_up)
+        down_force = hoomd.md.force.Constant(filter=self.shift_down)
+        up_force.constant_force[self.state.particle_types] = (
+                self.shear_axis * self.shear_force 
+        )
+        down_force.constant_force[self.state.particle_types] = (
+                self.shear_axis * -self.shear_force 
+        )
+        self.add_force(up_force)
+        self.add_force(down_force)
+
+    def run_shear(self, kT, n_steps):
+        self.set_integrator_method(
+            integrator_method=hoomd.md.methods.NVE,
+            method_kwargs={"filter": self.integrate_group}
+        )
+        self.run(n_steps + 1)
+
+
 class Shear(Simulation):
     def __init__(
             self,
