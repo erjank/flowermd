476c159370da70bdf8bc0847f4726369e8d39f1e
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2023-05-02 12:17:16 -0600

    fix unit tests

--- hoomd_polymers/utils.py
+++ hoomd_polymers/utils.py
@@ -12,5 +12,5 @@ def scale_charges(charges, n_particles):
     net_charge = sum(charges)
     abs_charge = sum([abs(charge) for charge in charges])
     adjust = abs(net_charge) / n_particles
-    charges -= (abs(charge) * net_charge/abs_charge)
+    charges -= (abs(charges) * net_charge/abs_charge)
     return charges
